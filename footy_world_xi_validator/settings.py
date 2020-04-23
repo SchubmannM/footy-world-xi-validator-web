a34ab7937ac2ef800f5ba4eadd9f4a9630b40689
SchubmannM <schubi89@gmail.com>
2020-04-23 19:08:03 +0100

    Update Pipfile

--- footy_world_xi_validator/settings.py
+++ footy_world_xi_validator/settings.py
@@ -78,16 +78,9 @@ class Common(Configuration):
 
     # Database
     # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "postgres",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "db",
-            "PORT": 5432,
-        }
-    }
+    DATABASES = values.DatabaseURLValue(
+        "sqlite:///{}".format(os.path.join(BASE_DIR, "db.sqlite3"))
+    )
 
     # Password validation
     # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -162,4 +155,13 @@ class Production(Staging):
     The in-production settings.
     """
 
-    pass
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "db",
+            "PORT": 5432,
+        }
+    }
